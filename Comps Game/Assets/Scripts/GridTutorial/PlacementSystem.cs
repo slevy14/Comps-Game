788cceaf76c176d59c07eccbdefd837276b54721
slevy14 <sammyglevy@gmail.com>
2024-10-02 19:10:02 -0700

    fixed z bug for gridposition

--- Comps Game/Assets/Scripts/GridTutorial/PlacementSystem.cs
+++ Comps Game/Assets/Scripts/GridTutorial/PlacementSystem.cs
@@ -8,16 +8,17 @@ public class PlacementSystem : MonoBehaviour {
     [SerializeField] private GameObject mouseIndicator, cellIndicator;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Tilemap tilemap;
-    [SerializeField] private Vector3 gridOffset;
-
     void Update() {
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
-        Vector3Int gridPosition = tilemap.WorldToCell(mousePosition + gridOffset);
+        Vector3Int gridPosition = tilemap.WorldToCell(mousePosition);
+        gridPosition.z = 0;
+        // Debug.Log(gridPosition);
 
         if (tilemap.HasTile(gridPosition)) {
             cellIndicator.transform.position = tilemap.GetCellCenterWorld(gridPosition);
             mouseIndicator.transform.position = mousePosition;
         } else {
+            Debug.Log("no tile at " + gridPosition);
             cellIndicator.transform.position = new Vector3(0f, 0f, 1f);
             mouseIndicator.transform.position = new Vector3(0f, 0f, 1f);
         }
